9dda65be062a9a7ef89c787290f5a95a469d7d08
web3muhammad <web3muhammad@icloud.com>
2023-11-25 15:55:23 +0300

    s

--- script.js
+++ script.js
@@ -7,5 +7,4 @@ let usercard = document.getElementById("usercard");
 let username = document.getElementById("username");
 let p = document.createElement("p");
 
-ßß
 p.innerText = `${tg.WebAppInitData.user.id}`
